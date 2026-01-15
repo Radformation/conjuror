90fa129d27893907baa496f55e922bc4754fcef8
Hasan Ammar <hammar@radformation.com>
2026-01-15 10:21:44 -0500

    PR comment; add back HIDE_PLOTS env var.

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,7 +6,11 @@ import plotly.io as pio
 
 TEST_FILES_DIR = osp.abspath(osp.join(osp.dirname(__file__), "test_files"))
 
-HIDE_PLOTS = True
+HIDE_PLOTS = os.environ.get("HIDE_PLOTS", default="True").lower() in [
+    "true",
+    "1",
+    "yes",
+]
 
 if os.environ.get("CI") or HIDE_PLOTS:
     matplotlib.use("Agg")
