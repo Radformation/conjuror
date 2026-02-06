e57afd787453a03694ed1405c8b66d48260ab5ee
Joao Silveira <jsilveira@radformation.com>
2026-02-06 17:32:16 +0100

    Refactor WL to reuse OpenField logic.

--- tests/test_truebeam.py
+++ tests/test_truebeam.py
@@ -9,11 +9,10 @@ from conjuror.plans.truebeam import (
     DEFAULT_SPECS_TB,
     TrueBeamMachine,
     OpenField,
-    OpenFieldMLCMode,
+    MLCLeafBoundaryAlignmentMode,
     MLCTransmission,
     PicketFence,
     WinstonLutz,
-    WinstonLutzMLCMode,
     WinstonLutzField,
     DoseRate,
     MLCSpeed,
@@ -54,10 +53,10 @@ class TestOpenField:
     # There is a significant portion of these tests that are copied in WinstonLutz.
 
     OPENFIELD_MLC_PARAM = [
-        (-2.5, 2.5, OpenFieldMLCMode.EXACT),
-        (-2.4, 2.6, OpenFieldMLCMode.ROUND),
-        (-2.6, 2.6, OpenFieldMLCMode.INWARD),
-        (-2.4, 2.4, OpenFieldMLCMode.OUTWARD),
+        (-2.5, 2.5, MLCLeafBoundaryAlignmentMode.EXACT),
+        (-2.4, 2.6, MLCLeafBoundaryAlignmentMode.ROUND),
+        (-2.6, 2.6, MLCLeafBoundaryAlignmentMode.INWARD),
+        (-2.4, 2.4, MLCLeafBoundaryAlignmentMode.OUTWARD),
     ]
 
     @pytest.mark.parametrize("y1,y2,mlc_mode", OPENFIELD_MLC_PARAM)
@@ -89,10 +88,10 @@ class TestOpenField:
         assert jaw_y[1, 0] == y2 + 5
 
     OPEN_MLC_PARAM = [
-        OpenFieldMLCMode.EXACT,
-        OpenFieldMLCMode.ROUND,
-        OpenFieldMLCMode.INWARD,
-        OpenFieldMLCMode.OUTWARD,
+        MLCLeafBoundaryAlignmentMode.EXACT,
+        MLCLeafBoundaryAlignmentMode.ROUND,
+        MLCLeafBoundaryAlignmentMode.INWARD,
+        MLCLeafBoundaryAlignmentMode.OUTWARD,
     ]
 
     @pytest.mark.parametrize("mlc_mode", OPEN_MLC_PARAM)
@@ -140,14 +139,19 @@ class TestOpenField:
 
     @pytest.mark.parametrize("y1,y2", [(-0.1, 0), (0, 0.1)])
     def test_error_if_mlc_exact_and_not_possible(self, y1, y2):
-        procedure = OpenField(0, 1, y1, y2, mlc_mode=OpenFieldMLCMode.EXACT)
+        procedure = OpenField(0, 1, y1, y2, mlc_mode=MLCLeafBoundaryAlignmentMode.EXACT)
         with pytest.raises(ValueError):
             procedure.compute(DEFAULT_TRUEBEAM_HD120)
 
     @pytest.mark.parametrize("y1,y2", [(-0.1, 0), (0, 0.1)])
     def test_exact_is_ignored_if_defined_by_jaws(self, y1, y2):
         procedure = OpenField(
-            0, 1, y1, y2, mlc_mode=OpenFieldMLCMode.EXACT, defined_by_mlc=False
+            0,
+            1,
+            y1,
+            y2,
+            mlc_mode=MLCLeafBoundaryAlignmentMode.EXACT,
+            defined_by_mlc=False,
         )
         procedure.compute(DEFAULT_TRUEBEAM_HD120)
 
@@ -239,10 +243,10 @@ class TestWinstonLutz:
             assert b.beam_name == n
 
     WL_MLC_PARAM = [
-        (-2.5, 2.5, WinstonLutzMLCMode.EXACT),
-        (-2.4, 2.6, WinstonLutzMLCMode.ROUND),
-        (-2.6, 2.6, WinstonLutzMLCMode.INWARD),
-        (-2.4, 2.4, WinstonLutzMLCMode.OUTWARD),
+        (-2.5, 2.5, MLCLeafBoundaryAlignmentMode.EXACT),
+        (-2.4, 2.6, MLCLeafBoundaryAlignmentMode.ROUND),
+        (-2.6, 2.6, MLCLeafBoundaryAlignmentMode.INWARD),
+        (-2.4, 2.4, MLCLeafBoundaryAlignmentMode.OUTWARD),
     ]
 
     @pytest.mark.parametrize("y1,y2,mlc_mode", WL_MLC_PARAM)
@@ -304,14 +308,21 @@ class TestWinstonLutz:
 
     @pytest.mark.parametrize("y1,y2", [(-0.1, 0), (0, 0.1)])
     def test_error_if_mlc_exact_and_not_possible(self, y1, y2):
-        procedure = WinstonLutz(0, 1, y1, y2, mlc_mode=WinstonLutzMLCMode.EXACT)
+        procedure = WinstonLutz(
+            0, 1, y1, y2, mlc_mode=MLCLeafBoundaryAlignmentMode.EXACT
+        )
         with pytest.raises(ValueError):
             procedure.compute(DEFAULT_TRUEBEAM_HD120)
 
     @pytest.mark.parametrize("y1,y2", [(-0.1, 0), (0, 0.1)])
     def test_exact_is_ignored_if_defined_by_jaws(self, y1, y2):
         procedure = WinstonLutz(
-            0, 1, y1, y2, mlc_mode=WinstonLutzMLCMode.EXACT, defined_by_mlc=False
+            0,
+            1,
+            y1,
+            y2,
+            mlc_mode=MLCLeafBoundaryAlignmentMode.EXACT,
+            defined_by_mlc=False,
         )
         procedure.compute(DEFAULT_TRUEBEAM_HD120)
         jaw_y = procedure.beams[0].beam_limiting_device_positions["ASYMY"]
